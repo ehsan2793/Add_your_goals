8f6a910f7b74f0bf353dfb449cfe1dfb54433989
ehsan2793 <ehsan2793@gmail.com>
2022-02-18 17:34:01 -0800

    media added

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,7 @@
-
 import styled from 'styled-components';
 // import './Button.css';
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -10,17 +10,17 @@ const Button = styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
   &:focus {
-  outline: none;
-}
-
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
-
+    outline: none;
+  }
+  @media (min-width: 768px) {
+    width: auto;
+  }
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
 `;
-
 
 export default Button;
